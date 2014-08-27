418d9f59057b44bf4fac6a7c17b955728e701ada
Jerry Nummi <jerry@neo.com>
2014-08-27 15:28:57 -0400

    IT'S ALIIIIIVEst! IT'S ALIVE...

--- engines/video_card/app/assets/javascripts/video_card/views/overlays/video_card_overlay_view.js
+++ engines/video_card/app/assets/javascripts/video_card/views/overlays/video_card_overlay_view.js
@@ -1,6 +1,4 @@
 ETahi.VideoCardOverlayView=ETahi.OverlayView.extend({
-	templateName:'video_card/overlays/video_card_overlay'
-	layoutName:'layouts/overlay_layout'
-})
-
-
+  templateName: 'overlays/video_card_overlay',
+  layoutName: 'layouts/overlay_layout'
+});
