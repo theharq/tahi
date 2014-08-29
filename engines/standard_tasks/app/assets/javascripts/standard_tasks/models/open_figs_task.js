d45b96af9d1deeba6df908df905b7806bbd4513a
Mike Mazur + Andi Plantenberg <pairing+mm+ap@edgecase.com>
2014-08-29 11:34:04 -0400

    New card to add images to paper figures

---
+++ engines/standard_tasks/app/assets/javascripts/standard_tasks/models/open_figs_task.js
@@ -0,0 +1,5 @@
+var a = DS.attr;
+ETahi.OpenFigsTask = ETahi.Task.extend({
+  qualifiedType: "StandardTasks::OpenFigsTask",
+  openFigsHtml: a('string')
+});
---
+++ engines/standard_tasks/app/models/standard_tasks/open_figs_task.rb
@@ -0,0 +1,10 @@
+module StandardTasks
+  class OpenFigsTask < Task
+    title "OpenFigs Task"
+    role "author"
+
+    # def active_model_serializer
+    #   TaskSerializer
+    # end
+  end
+end
