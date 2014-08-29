db5aa128ba58db7d4151230feb605040c5c8da36
Mike Mazur + Andi Plantenberg <pairing+mm+ap@edgecase.com>
2014-08-29 13:28:05 -0400

    Filter images by Journal

--- engines/standard_tasks/app/assets/javascripts/standard_tasks/controllers/overlays/open_figs_overlay_controller.js
+++ engines/standard_tasks/app/assets/javascripts/standard_tasks/controllers/overlays/open_figs_overlay_controller.js
@@ -1,13 +1,47 @@
 ETahi.OpenFigsOverlayController = ETahi.TaskController.extend({
+  filter: null,
+
   figureUploadUrl: function() {
     return "/papers/" + this.get('litePaper.id') + "/figures";
   }.property('litePaper.id'),
+
+  journals: [
+    'PLoS Genet',
+    'PLoS Biol',
+    'PLoS ONE',
+    'PLoS Comput Biol',
+    'PLoS Negl Trop Dis'
+  ],
+
+  images: function() {
+    var html = this.get('openFigsHtml');
+    return $('article img', html).toArray().map(function(i) {
+      return Ember.Object.create({
+        alt: i.alt,
+        src: i.src
+      });
+    });
+  }.property('openFigsHtml'),
+
+  filteredImages: function() {
+    if (!this.get('filter')) return this.get('images');
+
+    var currentFilter = this.get('filter');
+    return this.get('images').filter(function(i) {
+      return i.get('alt').indexOf(currentFilter) >= 0;
+    });
+  }.property('filter'),
 
   actions: {
     addFigure: function(image) {
       var postUrl = "/papers/" + this.get('litePaper.id') + "/figures";
       $.post(postUrl, {url: image.src});
       image.set('added', true);
+    },
+
+    filterJournals: function(filter) {
+      console.log('===> filtering by:', filter);
+      this.set('filter', filter);
     }
   }
 });
