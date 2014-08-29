b133a277df50edd6f9fff8603ae3bb96324f8829
Mike Mazur + Andi Plantenberg <pairing+mm+ap@edgecase.com>
2014-08-29 13:28:05 -0400

    Filter images by Journal

--- engines/standard_tasks/app/assets/javascripts/standard_tasks/views/overlays/open_figs_overlay_view.js
+++ engines/standard_tasks/app/assets/javascripts/standard_tasks/views/overlays/open_figs_overlay_view.js
@@ -1,15 +1,4 @@
 ETahi.OpenFigsOverlayView = ETahi.OverlayView.extend({
   templateName: "standard_tasks/overlays/open_figs_overlay",
   layoutName: "layouts/overlay_layout",
-
-  setup: function() {
-    var html = this.controller.get('openFigsHtml');
-    var images = $('article img', html).toArray().map(function(i) {
-      return Ember.Object.create({
-        alt: i.alt,
-        src: i.src
-      });
-    });
-    this.set('images', images);
-  }.on('didInsertElement')
 });
