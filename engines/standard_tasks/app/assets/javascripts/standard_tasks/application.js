23e84b8f3344f4df261aa1eae216b52d49d095a1
Aaron Christy <aaron@phad.com>
2014-08-29 10:50:38 -0400

    Add hopscotch

--- engines/standard_tasks/app/assets/javascripts/standard_tasks/application.js
+++ engines/standard_tasks/app/assets/javascripts/standard_tasks/application.js
@@ -10,5 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require hopscotch/dist/js/hopscotch
 //= require_tree .
 //
