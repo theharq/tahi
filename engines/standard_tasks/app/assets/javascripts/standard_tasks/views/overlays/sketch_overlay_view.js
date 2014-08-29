a6740b2849d712ea671d73037f8a0ec46f20e63d
Jerry Nummi <jerry@neo.com>
2014-08-29 10:37:47 -0400

    [WIP]

---
+++ engines/standard_tasks/app/assets/javascripts/standard_tasks/views/overlays/sketch_overlay_view.js
@@ -0,0 +1,21 @@
+ETahi.SketchOverlayView = ETahi.OverlayView.extend({
+  templateName: "standard_tasks/overlays/sketch_overlay",
+  layoutName: "layouts/overlay_layout",
+
+  setup: function() {
+    var self = this;
+    var tools = this.$('.sketch-tools');
+
+    $.each(['#f00', '#ff0', '#0f0', '#0ff', '#00f', '#f0f', '#000', '#fff'], function() {
+      tools.append("<a class='sketch-control' href='#colors-sketch' data-color='" + this + "' style='background: " + this + ";'></a> ");
+    });
+
+    tools.append('<span>&nbsp; &nbsp;</span>');
+
+    $.each([3, 5, 10, 15], function() {
+      tools.append("<a href='#colors-sketch' class='sketch-control' data-size='" + this + "' style='background: #ccc'>" + this + "</a> ");
+    });
+
+    $('#colors-sketch').sketch();
+  }.on('didInsertElement')
+});
