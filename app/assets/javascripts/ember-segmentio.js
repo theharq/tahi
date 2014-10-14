9946e7920ff75bbc64466160dbc61d732b9865cb
Ryan Wold <pair+ryan@neo.com>
2014-10-14 14:50:53 -0700

    track events on ember view changes
    used https://github.com/rahim/ember-segmentio

---
+++ app/assets/javascripts/ember-segmentio.js
@@ -0,0 +1,69 @@
+ETahi.SegmentioMixin = Ember.Mixin.create({
+  pageHasAnalytics: function() {
+    return window.analytics && typeof window.analytics === "object";
+  },
+
+  logTrackingEnabled: function() {
+    return !!window.ENV && !!window.ENV.LOG_EVENT_TRACKING;
+  },
+
+  logTracking: function() {
+    ETahi.Logger.info('[Segment.io] ', arguments);
+  },
+
+  trackPageView: function(page) {
+    if (this.pageHasAnalytics()) {
+      if (!page) {
+        var loc = window.location;
+        page = loc.hash ? loc.hash.substring(1) : loc.pathname + loc.search;
+      }
+
+      analytics.page(page);
+    }
+
+    if (this.logTrackingEnabled()) {
+      this.logTracking('page view', page);
+    }
+  },
+
+  trackEvent: function(event, properties, options, callback) {
+    if (this.pageHasAnalytics()) {
+      analytics.track(event, properties, options, callback);
+    }
+
+    if (this.logTrackingEnabled()) {
+      this.logTracking(event, properties, options);
+    }
+  },
+
+  identifyUser: function(userId, traits, options, callback) {
+    if (this.pageHasAnalytics()) {
+      analytics.identify(userId, traits, options, callback);
+    }
+
+    if (this.logTrackingEnabled()) {
+      this.logTracking('identify user', traits, options);
+    }
+  },
+
+  aliasUser: function(userId, previousId, options, callback) {
+    if (this.pageHasAnalytics()) {
+      analytics.alias(userId, previousId, options, callback)
+    }
+
+    if (this.logTrackingEnabled()) {
+      this.logTracking('alias user', previousId, options);
+    }
+  }
+});
+ETahi.initializer({
+  name: "segmentio",
+
+  initialize: function(container, application) {
+    var router = container.lookup('router:main');
+    router.on('didTransition', function() {
+      this.trackPageView(this.get('url'));
+    });
+  }
+});
+ETahi.Router.reopen(ETahi.SegmentioMixin);
